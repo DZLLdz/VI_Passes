03ba319594c42ae57448aa35efb20ed5fd5572a3
DZLLdz <145698883+DZLLdz@users.noreply.github.com>
2024-08-13 22:03:23 +0500

    add dotenv

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask
 from models import db
 from routes import main
+from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('FSTR_DB_LOGIN')}:{os.getenv('FSTR_DB_PASS')}@{os.getenv('FSTR_DB_HOST')}:{os.getenv('FSTR_DB_PORT')}/{os.getenv('FSTR_DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
